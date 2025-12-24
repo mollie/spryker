94cec94702ad09579be105264b5e7a04c3063af3
DajtiEmanuel <edajtixiphias@gmail.com>
2025-12-24 15:36:00 +0100

    MOLSPRY-33 - changing parameters on the bridge

--- src/Mollie/Client/Mollie/Dependency/Service/MollieToUtilEncodingServiceBridge.php
+++ src/Mollie/Client/Mollie/Dependency/Service/MollieToUtilEncodingServiceBridge.php
@@ -21,34 +21,21 @@ class MollieToUtilEncodingServiceBridge implements MollieToUtilEncodingServiceIn
 
     /**
      * @param array<string, string> $value
-     * @param int|null $options
-     * @param int|null $depth
      *
      * @return string|null
      */
-    public function encodeJson(array $value, ?int $options = null, ?int $depth = null): ?string
+    public function encodeJson(array $value): ?string
     {
-        return $this->utilEncodingService->encodeJson($value, $options, $depth);
+        return $this->utilEncodingService->encodeJson($value);
     }
 
     /**
      * @param string $jsonValue
-     * @param bool $assoc
-     * @param int|null $depth
-     * @param int|null $options
      *
      * @return array<string, string>|null
      */
-    public function decodeJson(string $jsonValue, bool $assoc = false, ?int $depth = null, ?int $options = null): ?array
+    public function decodeJson(string $jsonValue): ?array
     {
-        if ($assoc === false) {
-            trigger_error(
-                'Param #2 `$assoc` must be `true` as return of type `object` is not accepted.',
-                E_USER_DEPRECATED,
-            );
-        }
-
-        /** @phpstan-var array<mixed>|null */
-        return $this->utilEncodingService->decodeJson($jsonValue, $assoc, $depth, $options);
+        return $this->utilEncodingService->decodeJson($jsonValue, true);
     }
 }
--- src/Mollie/Client/Mollie/Dependency/Service/MollieToUtilEncodingServiceInterface.php
+++ src/Mollie/Client/Mollie/Dependency/Service/MollieToUtilEncodingServiceInterface.php
@@ -7,22 +7,17 @@ namespace Mollie\Client\Mollie\Dependency\Service;
 
 interface MollieToUtilEncodingServiceInterface
 {
- /**
-  * @param array<string, string> $value
-  * @param int|null $options
-  * @param int|null $depth
-  *
-  * @return string|null
-  */
-    public function encodeJson(array $value, ?int $options = null, ?int $depth = null): ?string;
+    /**
+     * @param array<string, string> $value
+     *
+     * @return string|null
+     */
+    public function encodeJson(array $value): ?string;
 
-   /**
-    * @param string $jsonValue
-    * @param bool $assoc
-    * @param int|null $depth
-    * @param int|null $options
-    *
-    * @return array<string, string>|null
-    */
-    public function decodeJson(string $jsonValue, bool $assoc = false, ?int $depth = null, ?int $options = null): ?array;
+     /**
+      * @param string $jsonValue
+      *
+      * @return array<string, string>|null
+      */
+    public function decodeJson(string $jsonValue): ?array;
 }
