4f8d883ff461351d91f3bfbc93728dc636336637
javornovosel <javor.novosel@gmail.com>
2026-01-21 12:39:32 +0100

    MOLSRPY-42 doc block fix

--- src/Mollie/Zed/Mollie/Communication/Cache/MollieCacheInvalidatorInterface.php
+++ src/Mollie/Zed/Mollie/Communication/Cache/MollieCacheInvalidatorInterface.php
@@ -6,5 +6,8 @@ namespace Mollie\Zed\Mollie\Communication\Cache;
 
 interface MollieCacheInvalidatorInterface
 {
+    /**
+     * @return void
+     */
     public function invalidateCache(): void;
 }
